ae05b8b45e23326a66dfe29905f8fba1a84a4ddf
Olexandr <bilyakalexxx@gmail.com>
2023-07-17 17:31:07 +0300

    add folder to folder

--- creator.py
+++ creator.py
@@ -2,11 +2,17 @@ import os
 
 PROJECTNAME = input("input_project_name:")
 
+print ("path example :D:\\")
+
 INPUTPATH = input(r"input_your_path:")
 
 a = '\\'
 
-list = ['obj', 'ref', '3dsmax', 'maya',]
+b = 'Substance'
+
+list = ['Ref', 'External','Substance', 'Marmoset', 'AE', 'Premiere', 'Renders', ]
+
+list2 = ['tex', 'outer']
 
 path = INPUTPATH+a+PROJECTNAME
 
@@ -14,5 +20,22 @@ os.mkdir(path)
 
 for list in list:
     
-    os.mkdir (path+a+list)
+    os.makedirs (path + a + list)
 
+if list in list:
+    os.makedirs (path + a + b + a + list2[0] )
+    os.makedirs (path + a + b + a + list2[1] )
+
+    
+
+
+
+
+
+
+
+
+
+
+#if True:
+#    print ("Done")
